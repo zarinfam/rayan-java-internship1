475042604863ad4e6be09d7fcb8473a182ed459f
Saeed Zarinfam <zarinfam.s@gmail.com>
2016-09-18 16:53:56 +0430

    add MessageManager class.

--- src/main/java/com/rh/Internship/Main.java
+++ src/main/java/com/rh/Internship/Main.java
@@ -4,7 +4,10 @@ package com.rh.Internship;
  * @author Saeed Zarinfam
  */
 public class Main {
+
+    private static MessageManager messageManager = new MessageManager();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(messageManager.getHelloMessage());
     }
 }
---
+++ src/main/java/com/rh/Internship/MessageManager.java
@@ -0,0 +1,11 @@
+package com.rh.Internship;
+
+/**
+ * @author Saeed Zarinfam
+ */
+public class MessageManager {
+
+    public String getHelloMessage(){
+        return "Hello World!";
+    }
+}
